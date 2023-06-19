e91f2b618d9b09358672a6ee9b39e15d8d66c953
skyzen <skyzen18@gmail.com>
2023-06-19 12:56:37 -0400

    labtest changes

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "app_ami" {
+data "aws_ami" "labtest_ami" {
   most_recent = true
 
   filter {
@@ -14,11 +14,64 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+# Create VPC
+resource "aws_vpc" "labtest_vpc" {
+  cidr_block = "10.0.0.0/16" 
 
   tags = {
-    Name = "HelloWorld"
+    Name  = "labtest-vpc"
+	Owner = "ajain"
+  }
+}
+
+# Create subnet within the VPC
+resource "aws_subnet" "labtest_subnet" {
+  vpc_id     = aws_vpc.labtest_vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "labtest-subnet"
+	Owner = "ajain"
+  }
+}
+
+
+# Create EC2 instances
+resource "aws_instance" "labtest_instance" {
+  count         = var.instance_count
+  ami           = data.aws_ami.labtest_ami.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.labtest_subnet.id
+
+  tags = {
+    Name = "labtest-instance-${count.index + 1}"
+  }
+}
+
+# Create EBS volumes
+resource "aws_ebs_volume" "labtest_volume" {
+  count              = var.instance_count
+  availability_zone  = aws_subnet.labtest_subnet.availability_zone
+  size               = 20  # Provide volume size or make it a variable
+
+  tags = {
+    Name = "labtest-volume-${count.index + 1}"
+  }
+}
+
+# Attach EBS volumes to the instances
+resource "aws_volume_attachment" "labtest_attachment" {
+  count       = var.instance_count
+  device_name = "/dev/xvdf"  # Provide device name or make it a variable
+  volume_id   = aws_ebs_volume.labtest_volume[count.index].id
+  instance_id = aws_instance.labtest_instance[count.index].id
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "labtest_bucket" {
+  bucket = "labtest-bucket" 
+
+  tags = {
+    Owner = "ajain3"
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  default     = 2
+}
